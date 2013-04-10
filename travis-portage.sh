c0bb576ede33e36719a50d80cb423767b79bd239
Andrew Udvare <audvare@gmail.com>
2013-04-10 01:27:44 -0700

    Fix PATH

--- travis-portage.sh
+++ travis-portage.sh
@@ -6,5 +6,5 @@ mkdir -p "$PWD/prefix/tmp"
 wget -O "$BOOTSTRAP" "http://overlays.gentoo.org/proj/alt/browser/trunk/prefix-overlay/scripts/bootstrap-prefix.sh?format=txt"
 bash "$BOOTSTRAP" "$PWD/prefix/tmp" stage1 && \
   bash "$BOOTSTRAP" "$PWD/prefix" stage2 && \
-  export PATH="$PWD/prefix/usr/bin" && \
+  export PATH="$PWD/prefix/usr/bin:$PATH" && \
   bash "$BOOTSTRAP" "$PWD/prefix" stage3
