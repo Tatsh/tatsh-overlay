d9d53ed42135c6d2771a5a3c7639bb61a1622816
Andrew Udvare <audvare@gmail.com>
2016-03-20 17:02:14 -0700

    Add /etc/gcruft to CONFIG_PROTECT (patch)

--- update-gcruft.sh
+++ update-gcruft.sh
@@ -1,10 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 git rm -f -r app-portage/gcruft
-mkdir -p app-portage
+rm -fR app-portage/gcruft
+mkdir -p app-portage/gcruft
 pushd app-portage
 svn co https://svn.keksbude.net/repos/keks-overlay/app-portage/gcruft/
 find -iname '.svn' -exec rm -fvR {} \;
 popd
+
+for i in app-portage/gcruft/*.ebuild; do
+    patch -p1 "$i" update-gcruft-config-protect.patch
+done
 
 git add app-portage/gcruft
 git commit -m "Updated gcruft (via script)"
