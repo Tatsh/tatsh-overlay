06716729624aa2f181ab20d4b2f6e802f7f3ae33
Andrew Udvare <audvare@gmail.com>
2022-04-11 02:14:14 -0400

    app-misc/zwave-js-server: make path names consistent
    Package-Manager: Portage-3.0.30, Repoman-3.0.3
    RepoMan-Options: --force
    Signed-off-by: Andrew Udvare <audvare@gmail.com>

--- app-misc/zwave-js-server/files/zwave-js-server.config.js
+++ app-misc/zwave-js-server/files/zwave-js-server.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   logConfig: {
-    filename: '/var/log/zwave-js/zwave-js',
+    filename: '/var/log/zwave-js-server/zwave-js-server',
     forceConsole: false,
     logToFile: true,
     level: 'error',
   },
   storage: {
-    cacheDir: '/var/cache/zwave-js',
-    deviceConfigPriorityDir: '/var/lib/zwave-js',
+    cacheDir: '/var/cache/zwave-js-server',
+    deviceConfigPriorityDir: '/var/lib/zwave-js-server',
   },
   securityKeys: require('./zwave-js-server.keys.js'),
 }
