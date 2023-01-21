be1bbca6d8e29b98231039bf64a16edc013dd1c7
Andrew Udvare <audvare@gmail.com>
2023-01-21 10:10:57 -0500

    scripts/generate-use-local: lint

--- scripts/generate-use-local.sh
+++ scripts/generate-use-local.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cd "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/.."
+cd "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/.." || exit 1
 while IFS=$'\n' read -r name; do
     {
         xq -r '.pkgmetadata.use.flag[]|(."@name" + " - " + ."#text")' \
@@ -8,6 +8,6 @@ while IFS=$'\n' read -r name; do
                 "$name" 2> /dev/null
     } |
         sed -re "s|(.*)|$(dirname "${name:2}"): \1|"
-done < <(find -name 'metadata.xml' -exec grep -HF '<use>' {} \; |
+done < <(find . -name 'metadata.xml' -exec grep -HF '<use>' {} \; |
     cut -d ':' -f1) |
     sort -u > profiles/use.local.desc
