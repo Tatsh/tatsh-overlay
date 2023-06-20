cfc600de6a6279ff8527bce2e7f177d8ead61084
Andrew Udvare <audvare@gmail.com>
2023-06-20 19:29:45 -0400

    x11-misc/mimeo: lint the completion script
    Signed-off-by: Andrew Udvare <audvare@gmail.com>

--- x11-misc/mimeo/files/mimeo-completion.sh
+++ x11-misc/mimeo/files/mimeo-completion.sh
@@ -4,10 +4,10 @@ __mimeo_completion()
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD - 1]}"
-    dash_opts=$(mimeo -h | egrep -- '(^  -)' | tr , $'\n' | awk '{ print $1 }' | egrep '^-')
+    dash_opts=$(mimeo -h | grep -E -- '(^  -)' | tr , $'\n' | awk '{ print $1 }' | grep -E '^-')
     if [[ ${cur} == -* ]]; then
         opts="$dash_opts"
     fi
-    COMPREPLY=($(compgen -W "${opts}" -- ${cur}))
+		mapfile -t COMPREPLY < <(compgen -W "${opts}" -- "${cur}")
 }
 complete -F __mimeo_completion mimeo
