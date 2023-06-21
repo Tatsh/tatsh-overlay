c9a69db9301ac551b112b0319ac3ac0d84bb24ec
Andrew Udvare <audvare@gmail.com>
2023-06-20 20:11:48 -0400

    scripts/autocommit-modified: process new metadata.xml

--- scripts/autocommit-modified.sh
+++ scripts/autocommit-modified.sh
@@ -21,3 +21,17 @@ while read -r ebuild; do
 		pkgdev commit || pkgdev commit -m "${cat}/${pn}: lint" || exit 1
     popd || exit 1
 done < <(git status | grep -E 'modified:.*(ebuild|metadata\.xml)$' | awk '{ print $2 }')
+while read -r xml; do
+    dn=$(dirname "$xml")
+    cat=$(dirname "$dn")
+    pn=$(basename "$dn")
+    pushd "$dn" || exit 1
+    if grep -qE 'soundfile' ./*.ebuild; then
+        popd || exit 1
+        continue
+    fi
+    ebuild ./*.ebuild clean manifest prepare || exit 1
+		git add .
+		pkgdev commit || pkgdev commit -m "${cat}/${pn}: add metadata" || exit 1
+    popd || exit 1
+done < <(git status | grep -E 'new file:.*metadata\.xml$' | awk '{ print $3 }')
