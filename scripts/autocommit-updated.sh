811430eb3b31f088efcaf3317c30151ca7ced77f
Andrew Udvare <audvare@gmail.com>
2025-05-30 22:19:35 -0400

    scripts/autocommit-updated: just install

--- scripts/autocommit-updated.sh
+++ scripts/autocommit-updated.sh
@@ -1,23 +1,11 @@
 #!/usr/bin/env bash
-contains-element() {
-    local e match="$1"
-    shift
-    for e; do [[ $e == "$match" ]] && return 0; done
-    return 1
-}
 cd "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/.." || exit 1
-test_install=(
-    games-arcade/outfox
-)
 while read -r ebuild; do
     dn=$(dirname "$ebuild")
     cat=$(dirname "$dn")
     pn=$(basename "$dn")
     pushd "$dn" || exit 1
-    phases=(clean manifest prepare)
-    if contains-element "${cat}/${pn}" "${test_install[@]}"; then
-        phases+=(install)
-    fi
-    ebuild ./*.ebuild "${phases[@]}" && git add . && pkgdev commit
+    phases=(clean manifest install)
+    ebuild ./*.ebuild clean manifest install && git add . && pkgdev commit
     popd || exit 1
 done < <(git status | grep -E 'deleted:.*ebuild$' | awk '{ print $2 }')
