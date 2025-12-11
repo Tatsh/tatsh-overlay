b80f933a555fa9b04c0e49b25ac1595a26761997
Andrew Udvare <audvare@gmail.com>
2025-12-10 21:09:49 -0500

    scripts/livecheck: update excludes

--- scripts/livecheck.sh
+++ scripts/livecheck.sh
@@ -1,47 +1,26 @@
 #!/usr/bin/env bash
 cd "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/.." || exit 1
 exceptions=(
-    # Need to keep older version for yt-dlp
-    #-e dev-python/curl-cffi
     # Needs handler
     -e dev-util/ida-free
-    # Keeps downgrading for some reason
-    #-e dev-util/vscode-vsce
     # Keep stable version
     -e games-arcade/outfox
-    # Needs handler
-    #-e net-print/epson-printer-utility
-    # Need to stay behind for kokoro-fastapi and tabbyAPI.
-    #-e dev-libs/marisa
-    #-e dev-python/catalogue
-    #-e dev-python/curated-tokenizers
-    #-e dev-python/curated-transformers
-    #-e dev-python/fugashi
-    #-e dev-python/openai
-    #-e dev-python/preshed
-    #-e dev-python/rfc3986
-    #-e dev-python/smart-open
-    #-e dev-python/spacy-curated-transformers
-    #-e dev-python/thinc
     # Not yet.
     -e app-admin/padd
-    -e app-admin/padd
-    -e app-admin/padd
-    -e app-admin/padd
-    -e net-dns/pihole-ftl
-    -e net-dns/pihole
-    -e www-apps/pihole-admin-lte
     -e games-emulation/gopher64
+    -e net-dns/pihole
+    -e net-dns/pihole-ftl
+    -e www-apps/pihole-admin-lte
     # Inaccurate.
     -e app-emulation/basiliskii
     -e app-emulation/sheepshaver
+    -e dev-python/thinc
     -e dev-qt/qtwebkit
-    -e games-emulation/cemu
     -e games-arcade/stepmania
+    -e games-emulation/cemu
     -e games-emulation/mupen64plus-video-gliden64
     -e games-emulation/rpcs3
     -e media-video/vapoursynth
-    -e dev-python/thinc
     # Weird tag livecheck cannot handle.
     -e games-emulation/fuse-libretro
     # Yarn packages broken?
