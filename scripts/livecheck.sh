53b88990adcded7e39762e5ec1a78f8ff4e15fde
Andrew Udvare <audvare@gmail.com>
2026-08-02 21:04:45 -0400

    scripts/livecheck: update
    Signed-off-by: Andrew Udvare <audvare@gmail.com>

--- scripts/livecheck.sh
+++ scripts/livecheck.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 cd "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/.." || exit 1
 exceptions=(
-    # Cannot uupdate yet.
-    -e dev-python/thinc
-    # Inaccurate.
-    -e games-emulation/cemu
-    # Needs a patch to use zstd from pkg-config not CMake.
-    -e games-emulation/mupen64plus-video-gliden64
-    # Upgrade handled incorrectly.
-    -e media-video/vapoursynth
     # Weird tag livecheck cannot handle.
     -e games-emulation/fuse-libretro
     -e games-emulation/pcsx-rearmed-libretro
